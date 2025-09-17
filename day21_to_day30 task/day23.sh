a63eb09c5f5e13e2aedf09bfd44d28f94abba397
ayanjyoti17 <jyotiayan47@gmail.com>
2025-09-17 20:33:27 +0530

    day 23 task added

---
+++ day21_to_day30 task/day23.sh
@@ -0,0 +1,12 @@
+# Day 23: Fork a Git Repository
+# There is a Git server utilized by the Nautilus project teams. Recently, a new developer named 
+# Jon joined the team and needs to begin working on a project. To begin, he must fork an existing 
+# Git repository. Follow the steps below:
+
+1. Click on the Gitea UI button located on the top bar to access the Gitea page.
+2. Login to Gitea server using username jon and password Jon_pass123.
+3. Once logged in, locate the Git repository named sarah/story-blog and fork it under the jon user.
+
+# solution :--->>>>>>>
+it should be done in the UI ..
+just follow the above question and do step by step .
