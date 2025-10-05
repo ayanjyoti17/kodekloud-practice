94d4ffe2ffc55ec5b4f2155e90d65d15cb892fbd
ayanjyoti17 <jyotiayan47@gmail.com>
2025-10-05 13:23:45 +0530

    day42 task added

---
+++ day41_to_day50 task/day42.sh
@@ -0,0 +1,15 @@
+# The Nautilus DevOps team needs to set up several docker environments for different applications. 
+# One of the team members has been assigned a ticket where he has been asked to create some docker networks 
+# to be used later. Complete the task based on the following ticket description:
+
+a. Create a docker network named as official on App Server 3 in Stratos DC.
+b. Configure it to use macvlan drivers.
+c. Set it to use subnet 192.168.30.0/24 and iprange 192.168.30.0/24.
+
+
+# Solution ->>>>>>>>>>>>>
+first ssh into App Server 3.
+docker network create -d macvlan --subnet=192.168.30.0/24 --ip-range=192.168.30.0/24 official
+
+# Verification
+docker network inspect official
