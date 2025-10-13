6ee6f274d461ede42fc63eeb433fb770f4c878bc
ayanjyoti17 <jyotiayan47@gmail.com>
2025-10-13 12:27:31 +0530

    day49 task added

---
+++ day41_to_day50 task/day49.sh
@@ -0,0 +1,13 @@
+# The Nautilus DevOps team is delving into Kubernetes for app management. One team member needs 
+# to create a deployment following these details:
+
+Create a deployment named nginx to deploy the application nginx using the image nginx:latest (ensure to specify the tag)
+Note: The kubectl utility on jump_host is set up to interact with the Kubernetes cluster.
+
+
+# Solution ->>>>>>>>>
+kubectl create deployment nginx --image=nginx:latest
+
+kubectl get deployments
+
+kubectl get pods
